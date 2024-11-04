46cc5dd8bb88d1e62eeb1f3d8666da347b51dfd5
Ellen Ribeiro <ellen.ribeiro@intactus.com.br>
2024-11-04 10:08:13 -0300

    Correcoes do PR e bloco sobre em andamento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { InicioComponent } from './components/inicio/inicio.component';
+import { SobreComponent } from './components/sobre/sobre.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, InicioComponent],
+  imports: [RouterOutlet, HeaderComponent, InicioComponent, SobreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
